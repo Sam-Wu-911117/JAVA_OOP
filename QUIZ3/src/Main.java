a842a51846a8b253bcdda6fd58e31e2f10f6631e
Sam-Wu-911117 <h4572306@gmail.com>
2022-12-05 23:31:01 +0800

    fill quiz3 ans

--- QUIZ3/src/Main.java
+++ QUIZ3/src/Main.java
@@ -5,26 +5,46 @@ public class Main {
         String str;
         boolean isPWFormatted;
         boolean isDigit;
-        //boolean isAalphabet;
         boolean isUpperCase;
-        boolean isLoweerCase;
+        boolean isLowerCase;
         boolean isBlank;
         boolean isNumberBeginning;
 
         do{
              isPWFormatted=false;
              isDigit=false;
-             //isAalphabet=false;
              isUpperCase =false;
-             isLoweerCase =false;
+             isLowerCase =false;
              isBlank=false;
              isNumberBeginning=false;
 
              System.out.println("請輸入密碼: ");
              str=sc.nextLine();
-
-             if(!isPWFormatted) System.out.println("請再輸入一次");
-             else System.out.println("密碼格式正確");
+              //程式
+             for (int i=0 ;i<str.length();i++){
+                 char a =str.charAt(i);
+                 if(!isNumberBeginning && i==0 && (a>='0' && a<='9')){
+                     isNumberBeginning =true;
+                 }
+                 if(!isDigit && (a>='0' && a<='9')){
+                     isDigit=true;
+                 }
+                 if(!isLowerCase && (a>='a' && a<='z')){
+                     isLowerCase=true;
+                 }
+                 if(!isUpperCase && (a>='A' && a<='Z')){
+                     isUpperCase=true;
+                 }
+                 if(!isBlank && a==' ')
+                     isBlank=true;
+             }
+             isPWFormatted=isDigit && isLowerCase && isUpperCase &&(!isNumberBeginning) &&(isBlank);
+             if(isPWFormatted)System.out.println("密碼格式正確");
+             if(isNumberBeginning)System.out.println("密碼不能數字開頭");
+             if(isBlank)System.out.println("密碼不能含空白");
+             if(!isDigit)System.out.println("密碼應該包含數字");
+             if(!isLowerCase)System.out.println("密碼應該包含小寫");
+             if(!isUpperCase)System.out.println("密碼應該包含大寫");
         }while(!isPWFormatted);
     }
 }
